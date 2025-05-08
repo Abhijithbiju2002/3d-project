70e4dec9dd95ba3a062248c565f16d09afb2c8c4
Abhijithbiju2002 <abhiju7@gmail.com>
2025-05-08 18:11:44 +0530

    commit 1 (b1)
    added parkouraction list

--- Assets/Parkour/ParkourController.cs
+++ Assets/Parkour/ParkourController.cs
@@ -1,17 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ParkourController : MonoBehaviour
 {
     public EnvironmentChecker environmentChecker;
+    bool playerInAction;
+    public Animator animator;
+
+    [Header("Parkour Action Area")]
+    public List<NewParkourAction> newParkourActions;
 
     private void Update()
     {
-        var hitData = environmentChecker.CheckObstacle();
-        //  environmentChecker.CheckObstacle();
-        if (hitData.hitFound)
+        if (Input.GetButton("Jump") && !playerInAction)
         {
-            Debug.Log("Object found " + hitData.hitInfo.transform.name);
+            var hitData = environmentChecker.CheckObstacle();
+            //  environmentChecker.CheckObstacle();
+            if (hitData.hitFound)
+            {
+
+            }
         }
+
 
     }
 }
